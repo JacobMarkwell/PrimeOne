a726e7955e8164dc1e4c4c08e5439df26e8f9e9c
JacobMarkwell <52938992+JacobMarkwell@users.noreply.github.com>
2024-02-12 20:58:50 -0600

    first commit

---
+++ Prime.cs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number:");
+        int number = int.Parse(Console.ReadLine());
+
+        bool isPrime = IsPrime(number);
+        Console.WriteLine($"{number} is {(isPrime ? "prime" : "non-prime")}");
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number <= 1)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
